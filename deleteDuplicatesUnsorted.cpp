6d5f5d2cf79e88383471652c613578df614a4fed
chandanagrawal23 <42837932+chandanagrawal23@users.noreply.github.com>
2021-12-13 17:50:57 +0530

    Update deleteDuplicatesUnsorted.cpp

--- deleteDuplicatesUnsorted.cpp
+++ deleteDuplicatesUnsorted.cpp
@@ -15,23 +15,23 @@ public:
         ListNode* ans = prev;  // to represent our final list
         ListNode* curr = head;
         unordered_map<int,int> M;  // to store values with their count
-        while(curr != nullptr) {
+        while(curr != NULL) {
             M[curr->val]++;
             curr = curr->next;
         }
         
         curr = head;
         
-        while(curr != nullptr) {
+        while(curr != NULL) {
             if(M[curr->val] > 1) {  // if value is found more than once
                 curr = curr->next;  // find next node with count == 1
             } else {
                 prev->next = curr;  //  assigning current unique node to the next of last unique node
-                prev = prev->next;  // current node becomes last uniquely occuring node
+                prev = curr;  // current node becomes last uniquely occuring node
                 curr = curr->next;
             }
         }
-        prev->next = curr;  // set null to the last of our list of unique value nodes
+        prev->next = NULL;  // set null to the last of our list of unique value nodes
         return ans->next;  // return the list 
     }
 };
