ac1078fe70ffefdf1b80012a93c13dd5c4a12c89
chandanagrawal23 <42837932+chandanagrawal23@users.noreply.github.com>
2025-05-11 19:46:19 +0530

    Update lengthOfLongestSubstringKDistinct.cpp

--- lengthOfLongestSubstringKDistinct.cpp
+++ lengthOfLongestSubstringKDistinct.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
     int lengthOfLongestSubstringKDistinct(string s, int k) {
-        map<char,int>mp;
-        int l = 0 , n = s.size() , r=0 ,ans = -1;
-        while(r<n)
-        {
-            mp[s[r]]++;
-            while(mp.size()>k)
-            {
-                mp[s[l]]--;
-                if(mp[s[l]]==0)
-                    mp.erase(s[l]);
-                l++;
+        map<char, int> mp;  // Stores character frequency in current window
+        int l = 0, r = 0, n = s.size(), ans = -1;
+
+        // Iterate through the string using right pointer `r`
+        while (r < n) {
+            mp[s[r]]++;  // Add character s[r] to map (or increment count)
+
+            // If number of distinct characters exceeds k, shrink window from left
+            while (mp.size() > k) {
+                mp[s[l]]--;  // Decrease count of character at left pointer
+                if (mp[s[l]] == 0)
+                    mp.erase(s[l]);  // Remove it from map if count becomes 0
+                l++;  // Move left pointer to right
             }
-            ans = max(ans , r-l+1);
-            r++;
+
+            // Update maximum length found so far
+            ans = max(ans, r - l + 1);
+
+            r++;  // Move right pointer to expand window
         }
+
         return ans;
     }
 };
