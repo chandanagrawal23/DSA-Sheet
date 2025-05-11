d1e321ce5257a1a264c8a53dceed9b15f845f597
chandanagrawal23 <42837932+chandanagrawal23@users.noreply.github.com>
2025-05-12 00:10:17 +0530

    Update MaximumScoreWordsFormedByLetters.cpp

--- MaximumScoreWordsFormedByLetters.cpp
+++ MaximumScoreWordsFormedByLetters.cpp
@@ -48,3 +48,62 @@ private:
         return maxScore;
     }
 };
+
+/*
+
+// Traditional Backtrack Method
+
+class Solution {
+public:
+    int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
+        vector<int> count(26, 0);
+        for (char ch : letters) {
+            count[ch - 'a']++;
+        }
+        return backtrack(words, count, score, 0);
+    }
+
+private:
+    int backtrack(vector<string>& words, vector<int>& count, vector<int>& score, int index) {
+        if (index == words.size()) return 0;
+
+        // Option 1: Don't pick the current word
+        int maxScore = backtrack(words, count, score, index + 1);
+
+        // Option 2: Try picking the current word
+        string word = words[index];
+        vector<int> used(26, 0);
+        int wordScore = 0;
+        bool canUse = true;
+
+        for (char ch : word) {
+            int idx = ch - 'a';
+            used[idx]++;
+            if (used[idx] > count[idx]) {
+                canUse = false;
+            }
+            wordScore += score[idx];
+        }
+
+        if (canUse) {
+            // Deduct used letters
+            for (int i = 0; i < 26; ++i) {
+                count[i] -= used[i];
+            }
+
+            // Take the word and add score
+            int withCurrent = wordScore + backtrack(words, count, score, index + 1);
+            maxScore = max(maxScore, withCurrent);
+
+            // Restore letter count
+            for (int i = 0; i < 26; ++i) {
+                count[i] += used[i];
+            }
+        }
+
+        return maxScore;
+    }
+};
+
+
+*/
