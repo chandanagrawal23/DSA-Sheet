cc4de507fbf5ff7bcea567c87cd7dd6e47b6cdfc
chandanagrawal23 <42837932+chandanagrawal23@users.noreply.github.com>
2025-07-14 16:40:11 +0530

    Update Permutations.cpp

--- Permutations.cpp
+++ Permutations.cpp
@@ -27,3 +27,56 @@ public:
 		}
     }
 };
+
+
+
+/*
+manual next_permutation too -
+
+bool manualNextPermutation(vector<int>& nums)
+{
+    int n = nums.size();
+
+    // Step 1: Find the first index 'i' such that nums[i] < nums[i + 1] from right to left
+    int i = n - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1])
+    {
+        i--;
+    }
+
+    // If no such index is found, the permutation is the last one
+    if (i < 0)
+    {
+        reverse(nums.begin(), nums.end());
+        return false;
+    }
+
+    // Step 2: Find the first index 'j' from the end such that nums[j] > nums[i]
+    int j = n - 1;
+    while (j > i && nums[j] <= nums[i])
+    {
+        j--;
+    }
+
+    // Step 3: Swap nums[i] and nums[j]
+    swap(nums[i], nums[j]);
+
+    // Step 4: Reverse the subarray from i + 1 to the end
+    reverse(nums.begin() + i + 1, nums.end());
+
+    return true;
+}
+int main()
+{
+    vector<int> nums = {1, 2, 3};
+
+    do {
+        for (int num : nums)
+            cout << num << " ";
+        cout << '\n';
+    } while (manualNextPermutation(nums));
+
+    return 0;
+}
+
+*/
